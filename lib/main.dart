eb5e7ea9d063a9b99138c9cf6f1ef0807c9ace31
crazytil <tilian.burger@outlook.com>
2022-02-20 14:56:32 +1000

    master

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:async_loader/async_loader.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:http/http.dart' as http;
+import 'package:v1/settings.dart';
+
+void main() {
+  runApp(application());
+}
+
+class application extends StatefulWidget {
+  const application({Key? key}) : super(key: key);
+
+  @override
+  _applicationState createState() => _applicationState();
+}
+
+double initial = 0;
+String initialvalueString = "";
+bool mode = false;
+
+class _applicationState extends State<application> {
+  int exhaust = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    var _asyncLoader = AsyncLoader(
+      initState: () async => await getMessage(),
+      renderLoad: () => CircularProgressIndicator(),
+      renderError: ([error]) =>
+          new Text('Sorry, there was an error loading your joke'),
+      renderSuccess: ({data}) => exhaustopen(),
+    );
+
+    return MaterialApp(
+        title: 'TBURG Exhaust Control',
+        home: Builder(
+            builder: (context) => Scaffold(
+                  appBar: AppBar(
+                    title: const Text('Exhaust Control'),
+                    backgroundColor: Colors.yellow,
+                    actions: <Widget>[
+                      IconButton(
+                        icon: Icon(
+                          Icons.settings,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SettingsPage()));
+                        },
+                      )
+                    ],
+                  ),
+                  body: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          _asyncLoader,
+                        ]),
+                  ),
+                )));
+  }
+}
+
+changeExhaust(int pwm) async {
+  pwm = (pwm * 2.55).round();
+  try {
+    http.Response response =
+        await http.get("http://192.168.3.30/analog/16/$pwm");
+    final jsonData = jsonDecode(response.body);
+    print(jsonData);
+  } catch (err) {
+    print(err);
+  }
+}
+
+getMessage() async {
+  const start = 'return_value": ';
+  const end = ",";
+  try {
+    http.Response response = await http.get("http://192.168.3.30/analog/16");
+    final responses = response.body;
+    final startIndex = responses.indexOf(start);
+    final endIndex = responses.indexOf(end, startIndex + start.length);
+    initialvalueString =
+        responses.substring(startIndex + start.length, endIndex);
+    initial = double.parse(initialvalueString) / 2.55;
+    print(initial);
+  } catch (err) {
+    print(err);
+  }
+
+  return (initial);
+}
+
+class exhaustopen extends StatefulWidget {
+  const exhaustopen({Key? key}) : super(key: key);
+
+  @override
+  _exhaustopenState createState() => _exhaustopenState();
+}
+
+class _exhaustopenState extends State<exhaustopen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(30),
+            child: SizedBox(
+              width: 200.0,
+              height: 100.0,
+              child: ElevatedButton(
+                child: mode
+                    ? Text("Passthrough Mode: ON")
+                    : Text("Passthrough Mode: OFF"),
+                onPressed: () {
+                  setState(() {
+                    mode = !mode;
+                    print(mode);
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30))),
+              ),
+            ),
+          ),
+          SleekCircularSlider(
+              initialValue: initial,
+              appearance: CircularSliderAppearance(
+                angleRange: 240,
+                size: 300,
+                customColors: CustomSliderColors(
+                    shadowColor: Colors.grey,
+                    trackColor: Colors.black,
+                    progressBarColor: Color(0xffe7e300)),
+              ),
+              onChangeEnd: (double value) {
+                changeExhaust(value.toInt());
+              }),
+        ]);
+  }
+}
---
+++ lib/settings.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
